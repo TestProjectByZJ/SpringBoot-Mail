e3578b7531ffe71e988f9f5ee6949ccfdc589544
zhoujian <zhoujian>
2019-04-17 17:19:42 +0800

    IDEA GIT TEST

---
+++ src/main/java/leven/texous/testmail/Test.java
@@ -0,0 +1,17 @@
+package leven.texous.testmail;
+
+/**
+ * @ProjectName: SpringBoot-Mail
+ * @Package: leven.texous.testmail
+ * @ClassName: Test
+ * @Author: zhoujian
+ * @Description: GIT测试
+ * @Date: 2019/4/17 0017 17:15
+ * @Version: 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("asdasd");
+    }
+}
--- src/main/java/leven/texous/testmail/TestMailApplication.java
+++ src/main/java/leven/texous/testmail/TestMailApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestMailApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestMailApplication.class, args);
 	}
 }
